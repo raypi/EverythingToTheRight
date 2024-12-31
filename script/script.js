194f2d777608656ffbc7caf3a6fae85c1a80824a
Ray <rainer.musch@gmx.de>
2024-12-31 05:38:37 +0100

    ad jason for playground

---
+++ script/script.js
@@ -0,0 +1,19 @@
+
+    let playground = {
+        "1": ["left", "left", "left", "left", "left", "left", "left"],
+        "2": ["left", "left", "left", "left", "left", "left", "left"],
+        "3": ["left", "left", "left", "left", "left", "left", "left"],
+        "4": ["left", "left", "left", "left", "left", "left", "left"],
+        "5": ["left", "left", "left", "left", "left", "left", "left"],
+        "6": ["left", "left", "left", "left", "left", "left", "left"],
+        "7": ["left", "left", "left", "left", "left", "left", "left"],
+        "8": ["left", "left", "left", "left", "left", "left", "left"],
+        "9": ["left", "left", "left", "left", "left", "left", "left"],
+        "10": ["left", "left", "left", "left", "left", "left", "left"],
+        "11": ["left", "left", "left", "left", "left", "left", "left"],
+        "12": ["left", "left", "left", "left", "left", "left", "left"]
+    }
+
+
+
+
